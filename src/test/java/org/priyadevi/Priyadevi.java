7f2f184be11eba2a4c5b46a8a3bafc879f60d98f
priyadevi12 <priyadevidinesh7@gmail.com>
2023-03-30 10:44:29 +0530

    day3task

--- src/test/java/org/priyadevi/Priyadevi.java
+++ src/test/java/org/priyadevi/Priyadevi.java
@@ -1,5 +1,11 @@
 package org.priyadevi;
 
 public class Priyadevi {
+	public static void main(String[] args) {
+		System.out.println("task3");
+		System.out.println("task4");
+		System.out.println("task5");
+		
+	}
 
 }
